661cbed98e3d7f481193efa6f72ad6d7f638d0b2
Igor Lukanin <mail@igor.lukanin.name>
2015-11-14 05:58:29 +0500

    Page 5 and page 6

--- app.js
+++ app.js
@@ -5,3 +5,60 @@ var mainView = app.addView('.view-main', {
     dynamicNavbar: true,
     domCache: true
 });
+
+var monthNames = [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь'
+];
+
+var calendarInline = app.calendar({
+    container: '#calendar-inline-container',
+    value: [new Date()],
+    weekHeader: false,
+    toolbarTemplate:
+    '<div class="toolbar calendar-custom-toolbar">' +
+    '<div class="toolbar-inner">' +
+    '<div class="left">' +
+    '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+    '</div>' +
+    '<div class="center"></div>' +
+    '<div class="right">' +
+    '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
+    '</div>' +
+    '</div>' +
+    '</div>',
+    onOpen: function (p) {
+        $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+        $$('.calendar-custom-toolbar .left .link').on('click', function () {
+            calendarInline.prevMonth();
+        });
+        $$('.calendar-custom-toolbar .right .link').on('click', function () {
+            calendarInline.nextMonth();
+        });
+    },
+    onMonthYearChangeStart: function (p) {
+        $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+    }
+});
+
+var ptrContent = $$('.pull-to-refresh-content');
+
+ptrContent.on('refresh', function (e) {
+    setTimeout(function () {
+        var block = ptrContent.find('.additional-info_warning');
+        block.removeClass('additional-info_warning');
+        block.addClass('additional-info_success');
+        block.text('Одобрено');
+        app.pullToRefreshDone();
+    }, 1000);
+});
